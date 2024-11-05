8e6f973cc774ef4a78e068c63d0fa939d10b0107
oscar2272 <oscar2272@naver.com>
2024-11-05 14:39:33 +0900

    .

--- interview/venv_interview/interview/interview/settings.py
+++ interview/venv_interview/interview/interview/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'NAME': get_env_variable('DB_NAME'),
         'USER': get_env_variable('DB_USER'),
         'PASSWORD': get_env_variable('DB_PASSWORD'),
-        'HOST': 'oscar2272',
+        'HOST': 'localhost',
         'PORT': get_env_variable('DB_PORT'),
     }
 }
