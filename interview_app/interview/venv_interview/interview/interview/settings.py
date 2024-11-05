f32d6f1fb217b969ff57405a16eb2f693d8b79bf
oscar2272 <oscar2272@naver.com>
2024-11-05 14:10:24 +0900

    ..

--- interview_app/interview/venv_interview/interview/interview/settings.py
+++ interview_app/interview/venv_interview/interview/interview/settings.py
@@ -36,8 +36,8 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".cloudtype.app"]
+CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
 
 # Application definition
 
