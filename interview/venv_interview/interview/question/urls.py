49b614c861afa97cdb7c61fa0529cf05024a3943
oscar2272 <oscar2272@naver.com>
2024-11-05 20:38:57 +0900

    ㅇㅇ

--- interview/venv_interview/interview/question/urls.py
+++ interview/venv_interview/interview/question/urls.py
@@ -6,12 +6,12 @@ from . import template_views
 app_name = 'questions'
 
 urlpatterns = [
-    path('', views.QuestionListView.as_view(), name='question_list'), #api view
-    path('<int:question_id>/',views.QuestionDetailView.as_view(), name='question_detail'),
+    path('/', views.QuestionListView.as_view(), name='question_list'), #api view
+    path('/<int:question_id>/',views.QuestionDetailView.as_view(), name='question_detail'),
     #/questions
 
-    path('upload_page/', views.view_uploaded_questions, name='view_uploaded_questions'),
-    path('delete_all/', template_views.delete_all_questions, name='delete_all_questions'),
-    path('<int:question_id>/', template_views.delete_question, name='delete_question'),
-    path('fill_other/', template_views.fill_empty_fields_with_other, name='fill_other'),
+    path('/upload_page/', views.view_uploaded_questions, name='view_uploaded_questions'),
+    path('/delete_all/', template_views.delete_all_questions, name='delete_all_questions'),
+    path('/<int:question_id>/', template_views.delete_question, name='delete_question'),
+    path('/fill_other/', template_views.fill_empty_fields_with_other, name='fill_other'),
 ]
