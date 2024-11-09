ce6e1b4261a047c446750cd51ca75faedf873a73
oscar2272 <oscar2272@naver.com>
2024-11-09 18:07:11 +0900

    Update settings.py

--- interview/venv_interview/interview/interview/settings.py
+++ interview/venv_interview/interview/interview/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".cloudtype.app"]
 CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
